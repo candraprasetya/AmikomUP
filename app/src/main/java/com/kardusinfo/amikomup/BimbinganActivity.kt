ceea08c766a468309acea0afa31f2df09c8c8f67
dexarya <arya.dexy@gmail.com>
2019-12-11 23:15:09 +0700

    ini bang

--- app/src/main/java/com/kardusinfo/amikomup/BimbinganActivity.kt
+++ app/src/main/java/com/kardusinfo/amikomup/BimbinganActivity.kt
@@ -3,10 +3,10 @@ package com.kardusinfo.amikomup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BimbinganActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_bimbingan)
     }
 }

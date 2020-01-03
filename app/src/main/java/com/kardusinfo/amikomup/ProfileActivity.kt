39122ca7e5d7a50d4d480113f132e4251ead004c
Dexy Arya <38392760+dexyarya@users.noreply.github.com>
2020-01-04 00:39:42 +0700

    belum fix2

--- app/src/main/java/com/kardusinfo/amikomup/ProfileActivity.kt
+++ app/src/main/java/com/kardusinfo/amikomup/ProfileActivity.kt
@@ -9,24 +9,12 @@ import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
 
-    lateinit var iconuser : EditText
-    lateinit var nim : EditText
-    lateinit var userImage : ImageView
-    lateinit var update : Button
-    lateinit var database : DatabaseReference
-    lateinit var Auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        iconuser = findViewById(R.id.iconUser)
-        userImage = findViewById(R.id.profile_image)
 
-        Auth = FirebaseAuth.getInstance()
-        val id  = Auth.currentUser.id
-
-        Database = FireDatabase.getIntance().getReference("user")
 
     }
 }

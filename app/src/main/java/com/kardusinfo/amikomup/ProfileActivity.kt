e18513825e54395a50c495e6da3b05c2b0cffa72
mahmudbro <mahmuddin.1399@students.amikom.ac.id>
2020-01-08 10:34:51 +0700

    done

--- app/src/main/java/com/kardusinfo/amikomup/ProfileActivity.kt
+++ app/src/main/java/com/kardusinfo/amikomup/ProfileActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuth
+import com.kardusinfo.amikomup.view.activities.WelcomeActivity
 import com.kardusinfo.amikomup.view.dashboard.DashboardCandra
 import kotlinx.android.synthetic.main.activity_dashboard_candra.*
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -23,7 +24,7 @@ class ProfileActivity : AppCompatActivity() {
             startActivity(
                 Intent(
                     this,
-                    DashboardCandra::class.java
+                    WelcomeActivity::class.java
                 ).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             )
 
